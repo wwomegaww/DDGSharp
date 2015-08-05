fe0b3ede44393db5c291b5f29738e0fcc915322a
xaxixeo <williamgonzalezalfaro@gmail.com>
2015-08-04 20:01:53 -0600

    minor fixes

--- AutoSharpporting 5.14/Autoplay/TreesAutoLevel.cs
+++ AutoSharpporting 5.14/Autoplay/TreesAutoLevel.cs
@@ -46,7 +46,7 @@ namespace Support
                     sequence = new[] { 2, 1, 3, 1, 1, 4, 1, 2, 1, 3, 4, 3, 3, 2, 2, 4, 2, 2 };
                     break;
                 case "Blitzcrank":
-                    sequence = new[] { 1, 3, 2, 3, 2, 4, 3, 2, 3, 2, 4, 3, 2, 1, 1, 4, 1, 1 };
+                    sequence = new[] { 1, 3, 1, 2, 1, 4, 1, 3, 1, 3, 4, 3, 3, 2, 2, 4, 2, 2 };
                     break;
                 case "Brand":
                     sequence = new[] { 2, 3, 2, 1, 2, 4, 2, 3, 2, 3, 4, 3, 3, 1, 1, 4, 1, 1 };
